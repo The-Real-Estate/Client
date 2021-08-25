e56c87b2b1227afeb5ae8512df1e65de3a6d8167
bishal76 <budhabishal280@gmail.com>
2021-08-25 18:12:24 +0545

    Permission success

--- app/src/main/java/com/bishal/therealestate/DashboardActivity.kt
+++ app/src/main/java/com/bishal/therealestate/DashboardActivity.kt
@@ -34,30 +34,30 @@ class DashboardActivity : AppCompatActivity() {
 //        )
 //        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        if (!hasPermission()) {
+            requestPermission()
+        }
 //
-//        if (!hasPermission()) {
-//            requestPermission()
-//        }
-//
-//    }
-//    private fun requestPermission() {
-//        ActivityCompat.requestPermissions(
-//            this@DashboardActivity,
-//            permissions, 1234
-//        )
-//    }
-//    private fun hasPermission(): Boolean {
-//        var hasPermission = true
-//        for (permission in permissions) {
-//            if (ActivityCompat.checkSelfPermission(
-//                    this,
-//                    permission
-//                ) != PackageManager.PERMISSION_GRANTED
-//            ) {
-//                hasPermission = false
-//            }
-//        }
-//        return hasPermission
-//    }
+    }
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            this@DashboardActivity,
+            permissions, 1234
+        )
+    }
+    private fun hasPermission(): Boolean {
+        var hasPermission = true
+        for (permission in permissions) {
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    permission
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                hasPermission = false
+            }
+        }
+        return hasPermission
+    }
     
 }
