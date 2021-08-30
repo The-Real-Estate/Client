be5b8ff3e438dcb84450cf2e71f983826eb1228e
bishal76 <budhabishal280@gmail.com>
2021-08-30 18:56:14 +0545

    instrumented testingcompleted

--- app/src/test/java/com/bishal/therealestate/MyTest.kt
+++ app/src/test/java/com/bishal/therealestate/MyTest.kt
@@ -7,25 +7,25 @@ import org.junit.Assert
 import org.junit.Test
 
 class MyTest {
-    private lateinit var userRepository :UserRepository
-    @Test
-    fun checkLogin() = runBlocking {
-        userRepository = UserRepository()
-        val response = userRepository.checkUser("bishal", "bishal")
-        val expectedResult = true
-        val actualResult = response.success
-        Assert.assertEquals(expectedResult, actualResult)
-    }
-    @Test
-    fun registerUser() = runBlocking {
-        val user =
-            User(FullName = "test", Address = "test",PhoneNo = "123456", Username = "testuser", Password = "testpassword")
-        userRepository = UserRepository()
-        val response = userRepository.registerUser(user)
-        val expectedResult = true
-        val actualResult = response.success
-        Assert.assertEquals(expectedResult, actualResult)
-    }
+//    private lateinit var userRepository :UserRepository
+//    @Test
+//    fun checkLogin() = runBlocking {
+//        userRepository = UserRepository()
+//        val response = userRepository.checkUser("bishal", "bishal")
+//        val expectedResult = true
+//        val actualResult = response.success
+//        Assert.assertEquals(expectedResult, actualResult)
+//    }
+//    @Test
+//    fun registerUser() = runBlocking {
+//        val user =
+//            User(FullName = "test", Address = "test",PhoneNo = "123456", Username = "testuser", Password = "testpassword")
+//        userRepository = UserRepository()
+//        val response = userRepository.registerUser(user)
+//        val expectedResult = true
+//        val actualResult = response.success
+//        Assert.assertEquals(expectedResult, actualResult)
+//    }
 
 
 }
